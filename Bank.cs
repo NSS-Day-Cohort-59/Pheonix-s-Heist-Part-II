945e0f53d04727401fe609b5ede617ec15216082
Samuel Trevan <samuel.trevan@gmail.com>
2022-12-07 09:41:05 -0600

    completed phaseone

---
+++ Bank.cs
@@ -0,0 +1,37 @@
+using System;
+
+namespace HeistPartII
+{
+    //Let's create a Bank class to represent the security we're up against. Give the Bank class the following properties:
+    public class Bank
+    {
+
+        //An integer property for CashOnHand
+        public int CashOnHand { get; set; }
+
+        //An integer property for AlarmScore
+        public int AlarmScore { get; set; }
+
+        //An integer property for VaultScore
+        public int VaultScore { get; set; }
+
+        //An integer property for SecurityGuardScore
+        //A computed boolean property called IsSecure. If all the scores are less than or equal to 0, this should be false. 
+        //If any of the scores are above 0, this should be true
+        public int SecurityGaurdScore { get; set; }
+        public bool IsSecure
+        {
+            get
+            {
+                if (AlarmScore <= 0 && VaultScore <= 0 && SecurityGaurdScore <= 0)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+        }
+    }
+}
