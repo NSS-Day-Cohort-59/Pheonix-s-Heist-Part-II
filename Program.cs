7ebbbf0d82823055ec090702ef00f7589209596b
Seaborn Mercer <surfermercer@gmail.com>
2022-12-07 13:03:34 -0600

    Almost done

--- Program.cs
+++ Program.cs
@@ -9,6 +9,36 @@ namespace HeistPartII
         {
 
 
+            
+            Bank TargetBank = new Bank()
+            {
+                CashOnHand = new Random().Next(50000, 1000000),
+                AlarmScore = new Random().Next(100),
+                VaultScore = new Random().Next(100),
+                SecurityGaurdScore = new Random().Next(100),
+            };
+            if(TargetBank.AlarmScore > TargetBank.VaultScore && TargetBank.AlarmScore > TargetBank.SecurityGaurdScore) {
+                Console.WriteLine("Most Secure: Alarm");
+            }
+            if(TargetBank.VaultScore > TargetBank.AlarmScore && TargetBank.VaultScore > TargetBank.SecurityGaurdScore) {
+                Console.WriteLine("Most Secure: Vault");
+            }
+            else {
+                Console.WriteLine("Most Secure: Security Guards");
+            }
+
+            if(TargetBank.AlarmScore < TargetBank.VaultScore && TargetBank.AlarmScore < TargetBank.SecurityGaurdScore) {
+                Console.WriteLine("Least Secure: Alarm");
+            }
+            if(TargetBank.VaultScore < TargetBank.AlarmScore && TargetBank.VaultScore < TargetBank.SecurityGaurdScore) {
+                Console.WriteLine("Least Secure: Vault");
+            }
+            else {
+                Console.WriteLine("Least Secure: Security Guards");
+            }
+
+            
+
             List<IRobber> rolodex = new List<IRobber>()
            {
                 new Hacker() {
@@ -44,40 +74,86 @@ namespace HeistPartII
         };
 
             Console.WriteLine($"You have {rolodex.Count} operatives.");
-            Console.WriteLine($"Enter the name of your new crew member.");
+            
+            while(true) {
+                Console.WriteLine($"Enter the name of your new crew member.");
 
-            string crewName = Console.ReadLine();
+                
 
-            string specialtyChoice = "";
-            while (specialtyChoice != "1" && specialtyChoice != "2" && specialtyChoice != "3")
-            {
+                string crewName = Console.ReadLine();
 
-                Console.WriteLine(@"Choose a specialty: 
+                if(crewName == "") {
+                    break;
+                }
 
-            1. Muscle
-            2. Hacker
-            3. Lock Specialist");
+                string specialtyChoice = "";
+                while (specialtyChoice != "1" && specialtyChoice != "2" && specialtyChoice != "3")
+                {
 
-                specialtyChoice = Console.ReadLine();
+                    Console.WriteLine(@"Choose a specialty: 
+
+                    1. Muscle
+                    2. Hacker
+                    3. Lock Specialist");
+
+                    specialtyChoice = Console.ReadLine();
+                }
+
+
+                int crewSkillLevel = 0;
+
+                while (crewSkillLevel < 1 || crewSkillLevel > 100)
+                {
+                    Console.WriteLine($"Please enter {crewName}'s skill level (1-100):");
+
+                    crewSkillLevel = int.Parse(Console.ReadLine());
+
+                }
+
+                int crewCutPercentage = 0;
+                while (crewCutPercentage < 1 || crewCutPercentage > 100)
+                {
+                    Console.WriteLine($"What is the percentage of {crewName}'s cut?");
+
+                    crewCutPercentage = int.Parse(Console.ReadLine());
+                }
+                IRobber larry;
+                
+                if(specialtyChoice == "1") {
+                    larry = new Muscle();
+                    
+                }
+                else if(specialtyChoice == "2") {
+                    larry = new Hacker();
+                    
+                }
+                else {
+                    larry = new LockSpecialist();
+                }
+                larry.Name = crewName;
+                larry.SkillLevel = crewSkillLevel;
+                larry.PercentageCut = crewCutPercentage;
+                rolodex.Add(larry);
+
+                Console.WriteLine($"You have {rolodex.Count} operatives.");
             }
 
 
-            int crewSkillLevel = 0;
 
-            while (crewSkillLevel < 1 || crewSkillLevel > 100)
-            {
-                Console.WriteLine($"Please enter {crewName}'s skill level (1-100):");
+            while(true) {
+                Console.WriteLine("Rolodex: ");
 
-                crewSkillLevel = int.Parse(Console.ReadLine());
+                for(int i = 0; i < rolodex.Count; i++) {
+                    Console.WriteLine(i + ". " + rolodex[i].Name + " " + rolodex[i].GetType().Name + " Skill Level: " + rolodex[i].SkillLevel + " Percent Cut: " + rolodex[i].PercentageCut);
+                }
 
-            }
+                List<IRobber> crew = new List<IRobber>();
 
-            int crewCutPercentage = 0;
-            while (crewCutPercentage < 1 || crewCutPercentage > 100) ;
-            {
-                Console.WriteLine($"What is the percentage of {crewName}'s cut?");
+                Console.WriteLine("Enter index of operative you would like to add to the heist: ");
+                int crewSelection = int.Parse(Console.ReadLine());
+                crew.Add(rolodex[crewSelection]);
 
-                crewCutPercentage = int.Parse(Console.ReadLine());
+                rolodex.Remove(rolodex[crewSelection]);
             }
 
 
